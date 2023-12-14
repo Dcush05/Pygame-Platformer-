d3f9518adfc712b3b515633928b7cd7b5a22f6c7
DylanC <dc11445p@pace.edu>
2023-12-14 11:31:09 -0500

    PROJECT VERSION

--- dist/assets/maps/spikes.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="spikes" tilewidth="20" tileheight="16" tilecount="48" columns="6">
- <image source="../spikes.png" width="128" height="128"/>
-</tileset>
